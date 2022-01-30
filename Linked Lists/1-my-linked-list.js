1a01373ddfd9878373ce17e0bb2197a8a508186e
Agniya Ivanishyna <ivanishyna@gmail.com>
2022-01-30 17:18:38 +0200

    Updated insert method of Linked List

--- Linked Lists/1-my-linked-list.js
+++ Linked Lists/1-my-linked-list.js
@@ -20,9 +20,64 @@ class MyLinkedList {
 
         return this;
     }
+
+    prepend(value) {
+        const node = {
+            value,
+            next: this.head
+        }
+
+        this.head = node;
+        this.length++;
+
+        return this;
+    }
+
+
+
+    insert(index, value) {
+        let currentNode = this.head;
+        let newNode = {
+            value,
+            next: null
+        }
+
+        for (let i = 0; i <= index; i++) {
+            if (i < index) {
+                currentNode = currentNode.next;
+            } else {
+                const pointer = currentNode.next;
+                currentNode.next = newNode;
+                newNode.next = pointer;
+            }
+        }
+
+    }
+
+    delete(index) {
+        //todo
+    }
+
+    print() {
+        const array = [];
+
+        let currentNode = this.head;
+
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        return array;
+
+    }
+
+
 }
 
 const myLinkedList = new MyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(18);
-console.log(myLinkedList);
+myLinkedList.append(6);
+myLinkedList.delete(2);
+console.log(myLinkedList.print());
